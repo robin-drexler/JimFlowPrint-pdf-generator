ce160ed283bc690f2284ff144c5a5967a46ea1a6
Robin Drexler <drexler.robin@gmail.com>
2014-09-06 19:15:52 +0200

    add jade, add hello world view

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
-  console.log(req.params);
-  res.send('Hello Worlds')
+
+app.get('/template', function (req, res) {
+  res.render('ticket', {
+    ticket: {
+      id: 23
+    }
+  });
 });
 
 app.listen(3000);
